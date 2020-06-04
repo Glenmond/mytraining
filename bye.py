aaa1014ba4e01041f76fe819012fdae5e4f22b0a
TWL98 <48822892+TWL98@users.noreply.github.com>
2020-06-04 23:42:39 +0800

    Update bye.py

--- bye.py
+++ bye.py
@@ -2,7 +2,7 @@ print("I love Acronis")
 print ("again")
 # need one more extra stuff
 # i want a new author
-print("I still love Wei Li even though I am angry")
+print("I love Wei Li")
 def sum(num):
     return num + 3
 sum(2)
