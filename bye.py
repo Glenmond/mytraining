b63577afcd93daca72f04f58399cd93c2fd08ce1
Glenmond <50542479+Glenmond@users.noreply.github.com>
2020-06-04 23:40:43 +0800

    Fixerror (#2)
    * New Changes
    
    * Created new file from weili
    
    * Fix Typo
    
    Co-authored-by: TWL98 <48822892+TWL98@users.noreply.github.com>

--- bye.py
+++ bye.py
@@ -1,5 +1,6 @@
 print("I love Acronis")
 print ("again")
+print("more items")
 # need one more extra stuff
 # i want a new author
 print("I still love Wei Li even though I am angry")
@@ -9,3 +10,4 @@ sum(2)
 #new stuff going on
 #new edit available
 print("ni hao")
+print("sup sup")
